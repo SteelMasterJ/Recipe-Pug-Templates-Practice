5c0c6a8255b3aef6c7cdac3c7f3328d457ad00c2
Joseph Steel <joseph.steel.iv@gmail.com>
2020-05-26 10:58:49 -0400

    Update for serving static assest
    update

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+//- 1. Add static middleware
+app.use('/static', express.static('public'));
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -26,11 +29,9 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
